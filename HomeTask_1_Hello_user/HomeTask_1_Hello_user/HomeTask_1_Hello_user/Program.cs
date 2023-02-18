0a1f787fcaa7163eb07ec822905aa0794b10722a
okarpuk <oleg.karpuk.1982@gmail.com>
2023-02-18 23:39:53 +0300

    Add project files.

---
+++ HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
