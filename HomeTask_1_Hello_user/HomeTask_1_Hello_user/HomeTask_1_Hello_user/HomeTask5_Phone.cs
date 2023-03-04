eb4e40854110ec59638bcea878c1eac35834d7d7
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-05 00:42:13 +0300

    Added part of task 1

---
+++ HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask5_Phone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
+{
+    public class HomeTask5_Phone
+    {
+        public int number = 0;
+        public string model = "Unknown";
+        public int weight = 0;
+        public string name = "Unknown";
+
+        //public HomeTask5_Phone() { number = 12345678; model = "qwerty"; weight = 222; }
+        //public HomeTask5_Phone() { number = 12345678; model = "qwerty"; }
+        //public HomeTask5_Phone() { }
+
+
+        //public HomeTask5_Phone(int number, string model) : this(number, model, 0)
+        //{
+
+        //}
+        //public HomeTask5_Phone(int number, string model, int weight)
+        //{
+        //    this.number = number;
+        //    this.model = model;
+        //    this.weight = weight;
+        //}
+
+        public void Print()
+        {
+            Console.WriteLine($"Phone number: {number}, Phone model: {model}, Phone weight: {weight}");
+        }
+
+        public void receiveCall()
+        {
+            Console.Write($"Caller: {name} ");
+        }
+
+        public void getNumber()
+        {
+            Console.WriteLine(number);
+        }
+
+
+
+
+
+
+    }
+}
--- HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/Program.cs
+++ HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
+﻿using System;
+
+namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            HomeTask3.HW3_T5_Change_odd_elements();
+            HomeTask5_Phone Xiaomi = new();
+            //Xiaomi.number = 1234567;
+            //Xiaomi.model = "Mi 12 Lite";
+            //Xiaomi.weight = 120;
+            Xiaomi.name = "John";
+            Xiaomi.Print();
+            Xiaomi.receiveCall();
+            Xiaomi.getNumber();
+
+            HomeTask5_Phone iPhone = new();
+            //iPhone.number = 9876543;
+            //iPhone.model = "14 pro";
+            //iPhone.weight = 154;
+            iPhone.name = "Zoe";
+            iPhone.Print();
+            iPhone.receiveCall();
+            iPhone.getNumber();
+
+            HomeTask5_Phone Samsung = new();
+            //Samsung.number = 1112233;
+            //Samsung.model = "Galaxy S23";
+            //Samsung.weight = 139;
+            Samsung.name = "Max";
+            Samsung.Print();
+            Samsung.receiveCall();
+            Samsung.getNumber();
+            
+
+
+
+
+
+
+
         }
     }
 }
