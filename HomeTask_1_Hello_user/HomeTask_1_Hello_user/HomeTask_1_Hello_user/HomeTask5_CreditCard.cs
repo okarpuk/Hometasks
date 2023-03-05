f0b0232a25341a8c2828a1e296dae6492059ae80
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-05 13:47:34 +0300

    Task 2 partially added

---
+++ HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask5_CreditCard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
+{
+    internal class HomeTask5_CreditCard
+    {
+        public int accountNumber = 123456;
+        public double currentSum = 3456.88;
+
+        public void Refill()
+        {
+            Console.WriteLine("Please enter your account number");
+            int enteredAccNumber = int.Parse(Console.ReadLine());
+            if (enteredAccNumber == accountNumber)
+            {
+                Console.WriteLine("Please enter sum to refill account");
+                double enteredSum = double.Parse(Console.ReadLine());
+                currentSum = currentSum + enteredSum;
+                Console.WriteLine($"Total sum on your account: {currentSum}");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect account number");
+            }
+        }
+
+        public void Withdrawal()
+        {
+            Console.WriteLine("Please enter your account number");
+            int enteredAccNumber = int.Parse(Console.ReadLine());
+            if (enteredAccNumber == accountNumber)
+            {
+                Console.WriteLine("Please enter sum to withdrawal");
+                double enteredSum = double.Parse(Console.ReadLine());
+                currentSum = currentSum - enteredSum;
+                Console.WriteLine($"Total sum on your account: {currentSum}");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect account number");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/Program.cs
+++ HomeTask_1_Hello_user/HomeTask_1_Hello_user/HomeTask_1_Hello_user/Program.cs
@@ -6,33 +6,42 @@ namespace HomeTask_1_Hello_user.HomeTask_1_Hello_user
     {
         static void Main(string[] args)
         {
-            HomeTask5_Phone Xiaomi = new();
-            Xiaomi.number = 1234567;
-            Xiaomi.model = "Mi 12 Lite";
-            Xiaomi.weight = 120;
-            Xiaomi.name = "John";
-            Xiaomi.Print();
-            Xiaomi.receiveCall();
-            //Xiaomi.receiveCall("Ivan"); //ДЛЯ ПЕРЕГРУЖЕННОГО МЕТОДА #1
-            Xiaomi.getNumber();
-            Xiaomi.sendMessage();
-            HomeTask5_Phone iPhone = new();
-            iPhone.number = 9876543;
-            iPhone.model = "14 pro";
-            iPhone.weight = 154;
-            iPhone.name = "Zoe";
-            iPhone.Print();
-            iPhone.receiveCall();
-            // iPhone.receiveCall("Joe", 445566); //ДЛЯ ПЕРЕГРУЖЕННОГО МЕТОДА #2
-            iPhone.getNumber();
-            HomeTask5_Phone Samsung = new();
-            Samsung.number = 1112233;
-            Samsung.model = "Galaxy S23";
-            Samsung.weight = 139;
-            Samsung.name = "Max";
-            Samsung.Print();
-            Samsung.receiveCall();
-            Samsung.getNumber();
+            // TASK _ PHONE
+            //HomeTask5_Phone Xiaomi = new();
+            //Xiaomi.number = 1234567;
+            //Xiaomi.model = "Mi 12 Lite";
+            //Xiaomi.weight = 120;
+            //Xiaomi.name = "John";
+            //Xiaomi.Print();
+            //Xiaomi.receiveCall();
+            ////Xiaomi.receiveCall("Ivan"); //ДЛЯ ПЕРЕГРУЖЕННОГО МЕТОДА #1
+            //Xiaomi.getNumber();
+            //Xiaomi.sendMessage();
+            //HomeTask5_Phone iPhone = new();
+            //iPhone.number = 9876543;
+            //iPhone.model = "14 pro";
+            //iPhone.weight = 154;
+            //iPhone.name = "Zoe";
+            //iPhone.Print();
+            //iPhone.receiveCall();
+            //// iPhone.receiveCall("Joe", 445566); //ДЛЯ ПЕРЕГРУЖЕННОГО МЕТОДА #2
+            //iPhone.getNumber();
+            //HomeTask5_Phone Samsung = new();
+            //Samsung.number = 1112233;
+            //Samsung.model = "Galaxy S23";
+            //Samsung.weight = 139;
+            //Samsung.name = "Max";
+            //Samsung.Print();
+            //Samsung.receiveCall();
+            //Samsung.getNumber();
+
+            // TASK - CREDIT CARD
+            HomeTask5_CreditCard RefillAccount = new();
+            RefillAccount.Refill();
+
+            HomeTask5_CreditCard WithdrawalAccount = new();
+            RefillAccount.Withdrawal();
+
         }
     }
 }
