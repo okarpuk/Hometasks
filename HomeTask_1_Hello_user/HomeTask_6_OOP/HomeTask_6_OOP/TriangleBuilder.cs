415f1d7c86661fd86746dc4a8837c6fe7dc28934
okarpuk <okarpuk@servicechannel.com>
2023-03-10 10:14:32 +0300

    Added one more part of Task1

--- HomeTask_1_Hello_user/HomeTask_6_OOP/HomeTask_6_OOP/TriangleBuilder.cs
+++ HomeTask_1_Hello_user/HomeTask_6_OOP/HomeTask_6_OOP/TriangleBuilder.cs
@@ -17,40 +17,46 @@ namespace HomeTask_6_OOP
                 return new EquiletarlTriangle(firstSide, secondSide, thirdSide);
             }
 
-            else if ((firstSide == secondSide) && (firstSide != thirdSide) && (secondSide != thirdSide))
-            {
-                return new IsoscelesTriangle(firstSide, secondSide, thirdSide);
-            }
-
             else if ((firstSide != secondSide) && (firstSide != thirdSide) && (secondSide != thirdSide))
             {
                 return new ScaleneTriangle(firstSide, secondSide, thirdSide);
             }
 
-
-
-
-            else if (firstSide == Math.Round(Math.Sqrt(Math.Pow(secondSide, 2) - (Math.Pow(thirdSide, 2))), 0));
+            else if ((firstSide == secondSide) && (firstSide != thirdSide))
             {
-                return new RightTriangle(firstSide, secondSide, thirdSide);
+                return new IsoscelesTriangle(firstSide, secondSide, thirdSide);
+            }
+            else if ((firstSide == thirdSide) && (firstSide != secondSide))
+            {
+                return new IsoscelesTriangle(firstSide, secondSide, thirdSide);
+            }
+            else if ((secondSide == thirdSide) && (firstSide != secondSide))
+            {
+                return new IsoscelesTriangle(firstSide, secondSide, thirdSide);
             }
 
+            double hypotenuse = firstSide;
+            double cathetFirst = secondSide;
+            double cathetSecond = thirdSide;
 
-
-
-
-           // else if (thirdSide == Math.Round(Math.Sqrt(Math.Pow(firstSide, 2) - (Math.Pow(secondSide, 2))), 0));
-           // {
-           //     return new RightTriangle(thirdSide, firstSide, secondSide);
-           // }
-
-
-
-
+            if (secondSide > hypotenuse)
+            {
+                hypotenuse = secondSide;
+                cathetFirst = firstSide;
+                cathetSecond = thirdSide;
+            }
+            if (thirdSide > hypotenuse)
+            {
+                hypotenuse = thirdSide;
+                cathetFirst = firstSide;
+                cathetSecond = secondSide;
+            }
+            if (Math.Pow(hypotenuse, 2) == Math.Pow(cathetFirst, 2) + Math.Pow(cathetSecond, 2))
+            {
+                return new RightTriangle(cathetFirst, cathetSecond, hypotenuse);
+            }
 
             return new Triangle(firstSide, secondSide, thirdSide);
-
         }
     }
 }
-
