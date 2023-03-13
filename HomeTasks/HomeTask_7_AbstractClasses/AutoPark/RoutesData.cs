969df4e09c6a5ab500c3fddab317662031e339cc
okarpuk <okarpuk@servicechannel.com>
2023-03-14 01:49:19 +0300

    Task Autopark added

---
+++ HomeTasks/HomeTask_7_AbstractClasses/AutoPark/RoutesData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace HomeTask_7_AbstractClasses
+{
+    public interface RoutesData
+    {
+        public void TransportInfo();
+    }
+}
---
+++ HomeTasks/HomeTask_7_AbstractClasses/AutoPark/SuperTransport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace HomeTask_7_AbstractClasses
+{
+    public abstract class SuperTransport
+    {
+        public abstract string TypeOfTransport();
+        public SuperTransport()
+        {
+        }
+    }
+}
